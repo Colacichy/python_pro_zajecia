114cb9a09a18fd2f9c0e4507772b9ea8ebfffbff
Colacichy <145496890+Colacichy@users.noreply.github.com>
2025-11-20 20:19:03 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+meme_dict = {
+            "CRINGE": "Coś wyjątkowo dziwnego lub zawstydzającego",
+            "LOL": "Częsta reakcja na coś zabawnego",
+            }
+word = input("Wpisz słowo, którego nie rozumiesz (używaj wielkich liter!): ")
