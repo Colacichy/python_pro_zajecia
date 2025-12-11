694e765e126d6bb5ff5580aaf5cc0a50112ce9fa
Colacichy <145496890+Colacichy@users.noreply.github.com>
2025-12-11 20:16:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-meme_dict = {
-            "CRINGE": "Coś wyjątkowo dziwnego lub zawstydzającego",
-            "LOL": "Częsta reakcja na coś zabawnego",
-            }
-word = input("Wpisz słowo, którego nie rozumiesz (używaj wielkich liter!): ")
+import discord
+from discord.ext import commands
+from bot_logic import gen_pass
+# Zmienna intents przechowuje uprawnienia bota
+intents = discord.Intents.default()
+# Włączanie uprawnienia do czytania wiadomości
+intents.message_content = True
+# Tworzenie bota w zmiennej client i przekazanie mu uprawnień
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Zalogowaliśmy się jako {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Cześć, jestem bot {bot.user}")
+
+@bot.command()
+async def haslo(ctx):
+    await ctx.send(gen_pass(10))
+
+bot.run("TOKEN")
